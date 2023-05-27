b2c09ab4c71a8505ce861413b8ad8fbd166bf50e
Vu Linh <vu.linh1807@gmail.com>
2023-05-27 10:47:51 +0700

    main

---
+++ Data/RazorPagesMoviesContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovies.Data
+{
+    public class RazorPagesMoviesContext : DbContext
+    {
+        public RazorPagesMoviesContext (DbContextOptions<RazorPagesMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+
+        public static implicit operator RazorPagesMoviesContext(Movie v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovies.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        //public ILogger<PrivacyModel> Logger => _logger;
+
+        public void OnGet()
+        {
+        }
+    }
+}
